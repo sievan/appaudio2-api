64f3f88b040e037af4492117177bd96a517b38b2
Jacob Sievers <jacosiev@gmail.com>
2017-06-28 11:42:53 +0200

    Add songs resource

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
 class SongsController < ApplicationController
   def index
   end
+
+  def create
+    render plain: params[:article].inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'songs/index'
+
+  resources :songs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
